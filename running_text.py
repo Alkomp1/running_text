df259acc1014cd3502db3a3fe5161fc2592e43f6
Alkomp1 <almaztolib@gmail.com>
2024-08-31 05:10:16 +0300

    Изменил шрифт

--- running_text.py
+++ running_text.py
@@ -16,14 +16,14 @@ def create_running_text_video(text, video_width=100, video_height=100, duration=
         frame = np.zeros((video_width, video_height, 3), dtype=np.uint8)
 
         # Вычисляем позицию текста
-        text_size = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, fontsize, 1)[0]
+        text_size = cv2.getTextSize(text, cv2.FONT_HERSHEY_COMPLEX, fontsize, 1)[0]
         text_width = text_size[0]
         text_height = text_size[1]
         x_position = video_width - int(i * (text_width + video_width) / num_frames)
         y_position = (video_height + text_height) // 2
 
         # Наносим текст на кадр
-        cv2.putText(frame, text, (x_position, y_position), cv2.FONT_HERSHEY_SIMPLEX, fontsize, color, 1, cv2.LINE_AA)
+        cv2.putText(frame, text, (x_position, y_position), cv2.FONT_HERSHEY_COMPLEX, fontsize, color, 1, cv2.LINE_AA)
 
         # Добавляем кадр в видео
         video.write(frame)
